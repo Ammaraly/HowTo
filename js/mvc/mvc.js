54cf7ea681f138457a9141e55cbc2fb94a69c005
Ammar Ali <m.ammarali98@gmail.com>
2021-08-05 23:12:46 +0500

    Added steps to create plugin

---
+++ js/mvc/mvc.js
@@ -0,0 +1,9 @@
+/**
+ * Add the route info
+ * Create an update view function
+ * wire up the update view function with url changes
+ * fetch the view html
+ * call the controller function
+ * process the view (replace the {{tokens}}) 
+ * render the view html in the view container
+ */
