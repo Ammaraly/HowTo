356d7a0139d041d13fab167aba9267469b7db574
Ammar Ali <m.ammarali98@gmail.com>
2021-08-08 00:51:09 +0500

    Adds another forEach implementation

--- js/es6/foreach.js
+++ js/es6/foreach.js
@@ -1,3 +1,12 @@
 var colors = ["red", "blue", "green", "purple", "yellow", "brown"];
 
+// Simple
 colors.forEach((color) => console.log(color));
+
+// With named function
+colors.forEach(someFunc);
+
+// Will print out colors rendered with background colors on browser console
+function someFunc(a) {
+    console.log(`%c${a}`, `background-color:${a};color:white`);
+}
