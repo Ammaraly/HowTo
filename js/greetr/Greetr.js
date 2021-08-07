9226ccdbfcda37e894066d53733328f8f2a72e79
Ammar Ali <m.ammarali98@gmail.com>
2021-08-07 21:19:55 +0500

    Added constructor and jquery

--- js/greetr/Greetr.js
+++ js/greetr/Greetr.js
@@ -1,3 +1,18 @@
 (function(global, $) {
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
 
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName = "John", lastName = "Doe", language = "en") {
+        var self = this;
+        self.firstName = firstName;
+        self.lastName = lastName;
+        self.language = language;
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
 })(window, jQuery);
