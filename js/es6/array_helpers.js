0240d95011f4231776e5e3de960b61715a6545b6
Ammar Ali <m.ammarali98@gmail.com>
2021-08-08 01:07:16 +0500

    Added map implementation

--- js/es6/array_helpers.js
+++ js/es6/array_helpers.js
@@ -1,12 +1,18 @@
 var colors = ["red", "blue", "green", "purple", "yellow", "brown"];
 
+//ForEach Helper
 // Simple
-colors.forEach((color) => console.log(color));
+// colors.forEach((color) => console.log(color));
 
 // With named function
-colors.forEach(someFunc);
+// colors.forEach(someFunc);
 
 // Will print out colors rendered with background colors on browser console
 function someFunc(a) {
     console.log(`%c${a}`, `background-color:${a};color:white`);
 }
+
+// Map Helper
+var colorsCapitalized = colors.map((color) => (color[0].toUpperCase() + color.slice(1)));
+
+console.log(colorsCapitalized);
