8a6033078bffd26a71d1f27b4f00fe55191296d9
Ammar Ali <m.ammarali98@gmail.com>
2021-08-08 23:18:42 +0500

    Adds a simple generator example

--- js/es6/destructuring.js
+++ js/es6/destructuring.js
@@ -4,4 +4,32 @@ const Google = {
 
 const { locations: [, second] } = Google;
 
-console.log(second);
+// console.log(second);
+const iterable = {
+    [Symbol.iterator]: function* iteratTeam() {
+        for (prop of Object.getOwnPropertyNames(this)) {
+            if (typeof this[prop] === 'object') yield* this[prop]
+            else yield this[prop];
+        }
+    }
+}
+
+const testingTeam = {
+    lead: "Roberta Mosciski",
+    testers: ["Kelli Johnston", "Mack Rath"]
+}
+
+const Team = {
+    testingTeam,
+    lead: "Jo Abbott",
+    manager: "PWilbert Hilll",
+    engineers: ["Arnold Rohan", "Janet Renner", "Beth Lesch", "Glenda Anderson"]
+}
+Team.__proto__ = iterable;
+testingTeam.__proto__ = iterable;
+
+for (member of Team) {
+    console.log(member);
+}
+
+// console.log(Team.__proto__)
